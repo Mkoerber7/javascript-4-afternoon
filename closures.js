d8772b575dfc4f5530f9850b962061ab3b99bf1a
Michael Koerber <michaelkoerber@Michaels-MacBook-Pro.local>
2018-02-15 16:26:06 -0600

    Finished closures

--- closures.js
+++ closures.js
@@ -22,13 +22,13 @@ function outer() {
   Invoke outer saving the return value into another variable called 'inner'.
 */
   
-// Code Here
+var inner = outer();
 
 
 
 //Once you do that, invoke inner.
 
-//Code Here
+inner();
 
 
 
@@ -51,7 +51,8 @@ function callFriend(name) {
   (HINT: You will need to pass in arguments to both function invocations)
 */
 
-//Code Here
+var callJake = callFriend('Jake');
+callJake(435-555-9248);
 
 
 
@@ -61,16 +62,23 @@ function callFriend(name) {
   Write a function called makeCounter that makes the following code work properly.
 */
 
-//Code Here
+function makeCounter() {
+  var start = 0;
+  var add = function() {
+    start +=1;
+    return start;
+  }
+  return add;
+}
 
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -86,10 +94,15 @@ function callFriend(name) {
 */
 
 function counterFactory(value) {
-  // Code here.
 
   return {
+    inc: function() {
+      return value += 1;
+    }, 
 
+    dec: function() {
+      return value -= 1;
+    }
   };
 }
 
@@ -112,10 +125,12 @@ counter = counterFactory(10);
 function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
-  // code message function here.
+function message() {
+  return welcomeText + ' ' + firstname + ' ' + lastname + '.';
+}
 
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -143,10 +158,12 @@ var module = (function() {
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
-    // Code here.
+    publicMethod: function() {
+      return privateMethod();
+    }
   };
 })();
-
+module.publicMethod();
 
 
 ////////// PROBLEM 7 //////////
@@ -158,14 +175,21 @@ var module = (function() {
   takeAwayFromSecret should have a parameter that takes away from the secret number returning the updated secret number.
 */
 
-function secretNumber() {
+function secretNumber(val) {
   var secret = 143;
 
   return {
-    // Code here
+    addToSecret: function(val) {
+      secret += val;
+      return secret
+    },
+
+    takeAwayFromSecret: function(val) {
+      secret -= val;
+      return secret;
+    }
   };
 }
-
 
 
 ////////// PROBLEM 8 //////////
@@ -186,11 +210,19 @@ function secretNumber() {
   Fix the code below to log the desired output.
 */
 
+function iSnap(j) {
+  return function() {
+    console.log(j)
+  }
+}
+
 function timeOutCounter() {
   for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
-      console.log(i);
-    }, i * 1000);
+    setTimeout(iSnap(i), i * 1000);
   }
 }
 timeOutCounter();
+
+
+
+
